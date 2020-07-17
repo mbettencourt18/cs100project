d7d2683d31ec33d990e5cc611c1c58a9d40001d1
mbettencourt18 <michael18bettencourt@gmail.com>
2020-07-17 11:32:42 -0700

    factory

---
+++ Factory.hpp
@@ -0,0 +1,24 @@
+#ifndef __FACTORY_HPP__
+#define __FACTORY_HPP__
+
+#include "Player.hpp"
+#include "Pg.hpp"
+#include "Sg.hpp"
+#include "Sf.hpp"
+#include "Pf.hpp"
+#include "Cn.hpp"
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <list>
+#include "Player_Header.h"
+
+using namespace std;
+
+class Player;
+class
