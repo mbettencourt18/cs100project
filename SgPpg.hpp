bdd6c6a46f33b2dd232705528124f27b0f8a640b
Rararahul <58281823+rkatwala@users.noreply.github.com>
2020-07-18 21:44:55 -0700

    Update SgPpg.hpp

--- SgPpg.hpp
+++ SgPpg.hpp
@@ -6,8 +6,8 @@
 class SgPpg : public PpgBase{
         public:
                 SgPpg() : PpgBase() {};
-                double rate(Player* player){
-                        return 35 * player.GetPoints() / 30;
+                double rate(Player* x){
+                        return 35 * x->GetPoints() / 30;
                 }
 };
 
