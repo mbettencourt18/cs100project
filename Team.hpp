27bede87b0b6959594d758fd850550c3f2b61ed4
mbettencourt18 <michael18bettencourt@gmail.com>
2020-07-20 20:10:04 -0700

    roster to team

--- Team.hpp
+++ Team.hpp
@@ -19,10 +19,10 @@ class Team:public Base {
 		return rating / 5;
 	}
         string TeamName;
-  	vector<Player*> roster;
+  	vector<Player*> list;
 	void Display(){
 		for(int i = 0; i < 5; ++i){
-			list[i]->Display();
+			list.at(i)->Display();
 		}
 	}
 	void AddPlayer(Player* player, int pos){
